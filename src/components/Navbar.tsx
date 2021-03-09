7341d74659189cc9ffe013e3ecd8458767df9a3b
reciokaue <kaue.recio2@gmail.com>
2021-03-09 14:07:26 -0300

    SearchDisplayCorrect

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
+import { useRouter } from 'next/router'
 
 import { Container, Topside, Bottomside } from '../styles/Navbar';
 import { BiCookie } from 'react-icons/bi'
 import { IoMdSearch } from 'react-icons/io'
 
 const Navbar: React.FC = () => {
+  const router = useRouter()
+
   return (
     <Container>
         <Topside>
             <h1>Caseirinhos</h1>
             <BiCookie/>
         </Topside>
-        <Bottomside>
-            <input placeholder="O que deseja comer?" type="text"/>
-            <IoMdSearch/>
-        </Bottomside>
+        {
+          router.asPath === '/' && (
+            <Bottomside>
+                <input placeholder="O que deseja comer?" type="text"/>
+                <IoMdSearch/>
+            </Bottomside>
+          )
+        }
+       
     </Container>
   );
 }

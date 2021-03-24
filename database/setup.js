eeb5c0be746e84fb2ea04d12d8837085b6e7e3cd
reciokaue <kaue.recio2@gmail.com>
2021-03-24 20:05:48 -0300

    Setup

--- database/setup.js
+++ database/setup.js
@@ -4,7 +4,6 @@ const sqlite = require('sqlite');
 async function setup() {
     const db = await sqlite.open('./database/person.sqlite');
     await db.migrate({force: 'last'});
-    db.close()
 }
 
 setup();
